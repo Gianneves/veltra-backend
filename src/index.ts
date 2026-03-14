ea1c8a7be1b4372db9064bd78d31b23d71026aa0
Gian Neves <gianneves@hotmail.com>
2026-03-14 12:17:47 -0300

    add: axios, jwt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
+import { authRoutes } from './routes/auth.route.ts';
+dotenv.config()
 
 const app = express()
 
@@ -8,6 +10,8 @@ app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT
+
+app.use('/api/v1/auth/', authRoutes)
 
 app.listen(port, () => {
     console.log(`server is running at port ${port}`)
